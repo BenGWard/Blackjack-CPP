86e6778a6e76dfde1fb2820e191f8c8439085ce6
Ben Ward <ben.g.ward@gmail.com>
2018-06-24 10:07:36 -0500

    Basic functionality working

--- main.cpp
+++ main.cpp
@@ -57,10 +57,11 @@ private:
 		temp = new card();
 		temp->link = NULL;
 		temp->type = (rand() % 13) + 1;
-		assignValue(temp);
+		temp->value = assignValue(temp);
 		return temp;
 	}
 
+	//assigns the values to cards base on the card type
 	int assignValue(card *temp)
 	{
 		int val;
@@ -107,12 +108,28 @@ public:
 	int value()
 	{
 		int value = 0;
+		int aceCount = 0;
 		card *cur = head;
 
 		while (cur)
 		{
-			value += cur->value;
+			if (cur->type == 1)
+			{
+				value += 11;
+				aceCount++;
+			}
+			else
+				value += cur->value;
+			
 			cur = cur->link;
+		}
+
+		//if we have aces, and counting them all as 11 has us bust,
+		//try setting to 1
+		while (aceCount > 0 && value > 21)
+		{
+			value -= 10; //swap an ace from 11 to 1
+			aceCount--;
 		}
 
 		return value;
@@ -137,20 +154,103 @@ public:
 		cout << endl;
 	}
 
-	//bool isBlackjack
+	//determines if a hand has been dealt blackjack (only run with two cards dealt)
+	bool isBlackjack()
+	{
+		bool blackjack = false;
+		int handValue = head->value + end->value;
+		
+		//if the hand is a face card or ten and an ace, the value will be 11
+		if (handValue == 11)
+			blackjack = true;
 
-	//bool hasAces
+		return blackjack;
+	}
+
+	//determines if a hand has aces in it
+	bool hasAces()
+	{
+		bool aces = false;
+		card *cur = head;
+
+		while (cur)
+		{
+			if (cur->type == 1)
+				aces = true;
+
+			cur = cur->link;
+		}
+
+		return aces;
+	}
+
+	//plays the dealers hand
+	void dealerPlay()
+	{
+		int handValue = value();
+		bool aces = hasAces();
+		
+		if (handValue < 17 && !aces)
+			this->draw();
+		else if (handValue == 17 && aces)
+			this->draw();
+	}
 };
 
 int main()
 {
-	bool playertype = false;
-	handOfCards player = new handOfCards(playertype);
+	handOfCards *player = new handOfCards();
+	handOfCards *dealer = new handOfCards();
+	bool gameOver = false;
+	char hitOrStay;
 
-	for (int i = 0; i < 100; i++)
-		player.draw();
+	//deal first two cards to player and dealer
+	for (int i = 0; i < 2; i++)
+	{
+		player->draw();
+		dealer->draw();
+	}
 
-	player.print(false);
+	//check for blackjack
+	if (player->isBlackjack())
+	{
+		gameOver = true;
+		cout << "You got blackjack! You won!" << endl;
+	}
+	else if (dealer->isBlackjack())
+	{
+		gameOver = true;
+		cout << "Dealer got blackjack. You lost." << endl;
+	}
+
+	while (!gameOver)
+	{
+		dealer->print(true);
+		player->print(false);
+
+		do
+		{
+			cout << "Do you want to hit or stay? [H / S]:";
+			cin >> hitOrStay;
+			if (tolower(hitOrStay) != 'h' && tolower(hitOrStay) != 's')
+				cout << "Please enter H or S." << endl;
+		} while (tolower(hitOrStay) != 'h' && tolower(hitOrStay) != 's');
+
+		if (tolower(hitOrStay) == 'h')
+		{
+			player->draw();
+			dealer->dealerPlay();
+		}
+		else
+		{
+			dealer->dealerPlay();
+			gameOver = true;
+		}
+	}
+
+	cout << "Final results:" << endl; 
+	cout << "Dealer: " << dealer->value() << endl;
+	cout << "Player: " << player->value();
 
 	system("pause");
 
